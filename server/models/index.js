4adbe118f7dee55025d198eaca678e47529e111e
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2023-03-02 10:51:40 -0700

    started working on schema, models,apollo, dealing with dependcy conflicts

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const Tech = require('./Tech');
-const Matchup = require('./Matchup');
+const Food = require('./Food');
+const User = require('./User');
 
-module.exports = { Tech, Matchup };
+module.exports = { User, Food };
