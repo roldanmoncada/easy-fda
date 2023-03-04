debe42a45409149e1863796c809cfec1bc9fead7
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2023-03-04 16:05:42 -0700

    connecting graphql queries to frontend working with apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import App from "./App";
- 
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+const link = createHttpLink({
+  uri: 'http://localhost:3001'
+});
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+
   </React.StrictMode>
 );
