12d3d242fee7457f42e5e446c40d72350da63ae4
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2023-03-04 17:59:01 -0700

    switched appolo provider to App.js, tried to get graphql data to frontend, not working yet

--- client/src/index.js
+++ client/src/index.js
@@ -3,35 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import App from "./App";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
- const httpLink = createHttpLink({
- uri: '/graphql'
-});
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+ 
  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
+   
       <App />
-    </ApolloProvider>
+   
 
   </React.StrictMode>
 );
