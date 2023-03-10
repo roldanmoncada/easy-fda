c6a2199dab2eabdf133243740e4cf001a681cbc5
roldanmoncada <roldan.h.moncada@gmail.com>
2023-03-09 20:15:46 -0700

    another attempt at heroku deployment

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "mongodb+srv://roldan:zee4u11ikVeqy2uQ@cluster0.8vto6kz.mongodb.net/?retryWrites=true&w=majority",
+  uri: "/graphql",
 });
 
 // const errorLink = onError(({ graphQLErrors, networkError }) => {
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://roldan:zee4u11ikVeqy2uQ@cluster0.8vto6kz.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/easy-fda', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
